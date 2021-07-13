461eb2ddcb591678bb1bd17379138926a0454584
Diwakar <diwakarn1599@gmail.com>
2021-07-13 21:21:41 +0530

    [Diwakar] Insertion sort

---
+++ DsAlgorithmProblems/InsertionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DsAlgorithmProblems
+{
+    class InsertionSort
+    {
+        public static void InsertionSortString()
+        {
+            string[] strArr = { "in", "I", "Diwakar", "Tvs Next", "working", "am" };
+            int len = strArr.Length;
+            Console.WriteLine("Array of string ---> Before Sorting:\n");
+            foreach (string i in strArr)
+            {
+                Console.Write($"{i} ");
+            }
+            sort(strArr, len);
+        }
+
+        //Insertion sort 
+        public static void sort(string[] strArr,int len)
+        {
+            for (int i = 1; i < len; i++)
+            {
+                String temp = strArr[i];
+                int j = i - 1;
+                while (j >= 0 && temp.Length < strArr[j].Length)
+                {
+                    strArr[j + 1] = strArr[j];
+                    j--;
+                }
+                strArr[j + 1] = temp;
+            }
+            Console.WriteLine("\nArray of string ---> After Sorting:\n");
+            foreach (string i in strArr)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
--- DsAlgorithmProblems/Program.cs
+++ DsAlgorithmProblems/Program.cs
@@ -7,7 +7,7 @@ namespace DsAlgorithmProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Data Structure algorithm Problems");
-            BinarySearch.BinarySearchfile();
+            InsertionSort.InsertionSortString();
         }
     }
 }
