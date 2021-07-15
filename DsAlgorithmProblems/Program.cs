466cd8ea94cd720686162ff98577b18a11ed2bfa
Diwakar <diwakarn1599@gmail.com>
2021-07-15 23:02:34 +0530

    [Diwakar] Guess a number

--- DsAlgorithmProblems/Program.cs
+++ DsAlgorithmProblems/Program.cs
@@ -9,7 +9,7 @@ namespace DsAlgorithmProblems
         {
             Console.WriteLine("Data Structure algorithm Problems");
             Console.WriteLine("MENU");
-            Console.WriteLine("1.Permutation\n2.Binary Search\n3.Insertion Sort\n4.Bubble Sort\n5.Merge Sort\n6.Anagram\n7.Prime Within Range\n8.Anagram and palindrome of prime numbers\n9.Exit\nENTER AN OPTION");
+            Console.WriteLine("1.Permutation\n2.Binary Search\n3.Insertion Sort\n4.Bubble Sort\n5.Merge Sort\n6.Anagram\n7.Prime Within Range\n8.Anagram and palindrome of prime numbers\n9.Guess Number\n10.Exit\nENTER AN OPTION");
             switch (Convert.ToInt32(Console.ReadLine()))
             {
                 case 1:
@@ -45,6 +45,10 @@ namespace DsAlgorithmProblems
                     anagramAndPalindrome.CheckPrimeAnagramAndPalindrome();
                     break;
                 case 9:
+                    GuessNumber gn = new GuessNumber();
+                    gn.GuessANumber();
+                    break;
+                case 10:
                     Console.WriteLine("Exited");
                     break;
             }
